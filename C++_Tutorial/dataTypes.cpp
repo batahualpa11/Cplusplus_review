1c3eb087ee21d5abf826a92d28981026f03b9774
batahualpa11 <brianatahualpa@gmail.com>
2025-02-12 23:01:49 -0500

    added datatypes file

--- C++_Tutorial/dataTypes.cpp
+++ C++_Tutorial/dataTypes.cpp
@@ -1,2 +1,15 @@
 // placeholder
 // create a branch and push it into main branch HW (find a way to make a branch and explain it)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int items = 50;
+    float cost_per_item = 9.99;
+    float total = items * cost_per_item;
+    char currency = '$';
+
+    
+}
